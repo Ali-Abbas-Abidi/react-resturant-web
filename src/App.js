718dbd08b7551029bd517c9cc5eb535ca4cd7dae
Ali Abbas Abidi <smaliabbasa890@gmail.com>
2025-07-30 02:15:07 +0500

    Update App.js, remove menuApi.js, add Hooks folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Resturant from "./component/Basics/Resturant";
+// import Resturant from "./component/Basics/Resturant";
+import UseReducer from "./component/Hooks/useReducer";
+
 const App = () => {
-  return <Resturant />;
+  return <UseReducer />;
 };
 
 export default App;
